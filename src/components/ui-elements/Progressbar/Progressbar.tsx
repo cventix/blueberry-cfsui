74f1767bfce65ab8c69394cd9c9cf027ab37c944
shaghz <sh.khangaldy@gmail.com>
2019-04-07 17:30:25 +0430

    changable width

--- src/components/ui-elements/Progressbar/Progressbar.tsx
+++ src/components/ui-elements/Progressbar/Progressbar.tsx
@@ -4,13 +4,15 @@ import styles from "./Progressbar.module.scss";
 interface Pprops {
   value: number;
   error?: boolean;
+  width?:number;
 }
 
-const Progressbar = ({ value, error = false, ...props }: Pprops) => (
+const Progressbar = ({ value, error = false,width, ...props }: Pprops) => (
   <div className={styles.progressContainer}>
     <progress
       value={value}
       max={100}
+      style={{width:width}}
       className={
         error ? styles.red : value === 100 ? styles.green : styles.yellow
       }
