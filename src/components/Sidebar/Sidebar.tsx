5080e10fda1f3bedaa5b8b6720742814bdffef12
Alipanah <alipanah@Mehdi.local>
2019-04-08 09:59:34 +0430

    sidebar cmp added

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import styles from "./Sidebar.module.scss";
 
 interface Iprops {
   text: string;
 }
 
 const Sidebar: React.FunctionComponent<Iprops> = props => {
-  return <div>{props.text}</div>;
+	return (
+		<aside></aside>
+	);
 };
 
 export default Sidebar;
--- src/containers/Home/App.tsx
+++ src/containers/Home/App.tsx
@@ -9,7 +9,6 @@ class App extends Component {
       <div>
         <Navbar text="arezoo"/>
       </div>
-      
     );
   }
 }
