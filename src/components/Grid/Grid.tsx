d164fea72bc13cf59485f07e742a020d00f60ff3
shaghz <sh.khangaldy@gmail.com>
2019-04-10 09:58:49 +0430

    update grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -5,12 +5,21 @@ import { Card } from "../ui-elements/Card/Card";
 export default interface Iprops {
   checkbox?: boolean;
   table: any;
+  isOpen?: boolean;
 }
 
 export const Grid = ({ table, checkbox }: Iprops) => {
   return (
-      <div className={styles.container}>
-          {table.map((item:any)=> { return <Card item={item} checkbox={true} dropdown={true}/> })}
-      </div>
+    <div className={styles.container}>
+      {table.map((item: any, index: number) => {
+        return (
+          <Card
+            item={item}
+            checkbox={true}
+            dropdown={index == 3 ? true : false}
+          />
+        );
+      })}
+    </div>
   );
 };
