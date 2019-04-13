5862ef0c61476fa7045d47748fc0a082d4d1b1ab
shaghz <sh.khangaldy@gmail.com>
2019-04-13 12:07:02 +0430

    styled toggle

--- src/components/ui-elements/Toggle/Toggle.tsx
+++ src/components/ui-elements/Toggle/Toggle.tsx
@@ -4,12 +4,23 @@ import styles from "./Toggle.module.scss";
 interface Iprops {
   checked: boolean;
   disabled?: boolean;
-  onToggle?: () => void;
+  onToggle?: (e: boolean) => void;
+  className?: string;
 }
-const Toggle: React.SFC<Iprops> = ({ checked, disabled, onToggle }) => (
-  <label className={disabled ? styles.gray : styles.switch}>
-    <input type="checkbox" checked={checked} />
-    <span className={styles.knob} />
-  </label>
+const Toggle: React.SFC<Iprops> = ({
+  checked,
+  disabled,
+  onToggle,
+  className
+}) => (
+  <div
+    className={className && styles[className]}
+    {...onToggle && { onClick: () => onToggle(checked) }}
+  >
+    <label className={disabled ? styles.gray : styles.switch}>
+      <input type="checkbox" defaultChecked={checked} />
+      <span className={styles.knob} />
+    </label>
+  </div>
 );
 export default Toggle;
