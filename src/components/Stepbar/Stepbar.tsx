ff92d0e7756b1f56e54efafde0cab6096081d79b
Alipanah <alipanah@Mehdi.local>
2019-04-13 14:41:59 +0430

    stepbar completed and checkbox/radio edited

--- src/components/Stepbar/Stepbar.tsx
+++ src/components/Stepbar/Stepbar.tsx
@@ -3,44 +3,37 @@ import * as React from "react";
 // styles
 import styles from "./Stepbar.module.scss";
 
-
 export default interface Iprops {
-	caption?: string;
+	steps: string[];
+	currentStep: number;
 }
 
-export const Stepbar = ({ caption }: Iprops) => (
+const renderSteps = (steps: string[], currentStep: number) => {
+	return steps.map((stepTitle, index) => {
+		let stepClassName = '';
+		currentStep = currentStep || 0;
+
+		if (index < currentStep)
+			stepClassName = `${styles.completed}`;
+
+		if (index === currentStep) 
+			stepClassName = `${styles.current}`;
+
+		return (
+			<li className={`${styles.step} ${stepClassName}`} key={index}>
+				<a className={styles.item}>
+					<span className={styles.circle}></span>
+					<span className={styles.caption}>{stepTitle}</span>
+				</a>
+			</li>
+		)
+	});
+};
+
+export const Stepbar = ({ steps, currentStep }: Iprops) => (
 	<div className={styles.stepbar}>
 		<ul className={styles.stepsWrapper}>
-			<li className={`${styles.completed} ${styles.step}`}>
-				<a className={styles.item}>
-					<span className={styles.circle}></span>
-					<span className={styles.caption}>انتخاب سیستم عامل</span>
-				</a>
-			</li>
-			<li className={`${styles.completed} ${styles.step}`}>
-				<a className={styles.item}>
-					<span className={styles.circle}></span>
-					<span className={styles.caption}>انتخاب مدت سرویس</span>
-				</a>
-			</li>
-			<li className={`${styles.current} ${styles.step}`}>
-				<a className={styles.item}>
-					<span className={styles.circle}></span>
-					<span className={styles.caption}>انتخاب طرح</span>
-				</a>
-			</li>
-			<li className={`${styles.step}`}>
-				<a className={styles.item}>
-					<span className={styles.circle}></span>
-					<span className={styles.caption}>اطلاعات کارت شبکه</span>
-				</a>
-			</li>
-			<li className={`${styles.step}`}>
-				<a className={styles.item}>
-					<span className={styles.circle}></span>
-					<span className={styles.caption}>انتخاب نام سرور و ثبت نهایی</span>
-				</a>
-			</li>
+			{renderSteps(steps, currentStep)}
 		</ul>
 	</div>
 );
--- src/containers/Home/App.tsx
+++ src/containers/Home/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Stepbar } from "../../components/Stepbar/Stepbar";
 import { Main } from "../../components/Main/Main";
 import "./App.css";
 import Table from "../../components/Table/Table";
@@ -92,13 +93,23 @@ const table = [
   }
 ];
 
+const steps = [
+  'انتخاب سیستم عامل',
+  'انتخاب مدت سرویس',
+  'انتخاب طرح', 
+  'اطلاعات کارت شبکه',
+  'انتخاب نام سرور و ثبت نهایی'
+]
+
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
         <Sidebar />
-        <Main>CONTENT</Main>
+        <Main>
+          <Stepbar steps={steps} currentStep={2}/>
+        </Main>
       </div>
     );
   }
