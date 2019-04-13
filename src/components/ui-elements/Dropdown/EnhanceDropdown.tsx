50f2453d26d7573d5f4f6a39fdc3151793a9179e
shaghz <sh.khangaldy@gmail.com>
2019-04-13 09:27:32 +0430

    update dropdown

--- src/components/ui-elements/Dropdown/EnhanceDropdown.tsx
+++ src/components/ui-elements/Dropdown/EnhanceDropdown.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 
 interface MyProps {
-  optionSelected: string;
   data: object;
-  onSelect: (e: string) => void;
 }
 
 interface MyState {
@@ -17,7 +15,6 @@ export const EnhanceDropdown = (ComposedComponent: any) =>
       this.state = { isOpen: false };
       this.onToggle = this.onToggle.bind(this);
       this.handleDocumentClick = this.handleDocumentClick.bind(this);
-      this.onSelect = this.onSelect.bind(this);
     }
 
     componentDidMount() {
@@ -37,20 +34,13 @@ export const EnhanceDropdown = (ComposedComponent: any) =>
       this.setState({ isOpen: !this.state.isOpen });
     }
 
-    onSelect(option: any) {
-      this.onToggle();
-      if (this.props.onSelect) this.props.onSelect(option);
-    }
-    
     render() {
       return (
         <div onClick={e => e.stopPropagation()}>
           <ComposedComponent
             {...this.props}
             onToggle={this.onToggle}
-            onSelect={this.onSelect}
             isOpen={this.state.isOpen}
-            optionSelected={this.props.optionSelected}
             data={this.props.data}
           />
         </div>
