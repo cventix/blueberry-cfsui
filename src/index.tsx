38e383f05384d5cf6111302dd211d6d3ff4c2874
Alipanah <alipanah@Mehdi.local>
2019-06-18 10:54:10 +0430

    back to without tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './services/internal/store/configStore'
-
 import App from './containers/Home/App'
 import * as serviceWorker from './serviceWorker'
 import Routes  from './Routes';
