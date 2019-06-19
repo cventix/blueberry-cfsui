56e79af7bcd48c7f30bd8e2684315b7380892eb8
Alipanah <alipanah@Mehdi.local>
2019-06-19 15:26:03 +0430

    refactor navar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,24 @@
 // TODO: if has notification add hasNotif classname
 // TODO: current page has current classname
 
-import * as React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import * as React from 'react'
 import { t } from 'ttag'
 
 // ui-elements
-import { Icon } from '../ui-elements/Icon'
 import { IconLink } from '../ui-elements/IconLink'
 import { Avatar } from '../ui-elements/Avatar'
+import { Icon } from '../ui-elements/Icon'
 
 // icons & styles
+import fileCloudIcon from '../../images/navbarIcons/file-cloud.svg'
+import internetIcon from '../../images/navbarIcons/internet.svg'
+import financeIcon from '../../images/navbarIcons/finance.svg'
+import statusIcon from '../../images/navbarIcons/status.svg'
+import notifIcon from '../../images/navbarIcons/notif.svg'
+import vpsIcon from '../../images/navbarIcons/vps.svg'
 import logo from '../../images/pg-logo.svg'
 import menu from '../../images/hmenu.svg'
-import fileCloudIcon from '../../images/navbarIcons/file-cloud.svg'
-import vpsIcon from '../../images/navbarIcons/vps.svg'
-import internetIcon from '../../images/navbarIcons/internet.svg'
-import statusIcon from '../../images/navbarIcons/status.svg'
-import financeIcon from '../../images/navbarIcons/finance.svg'
-import notifIcon from '../../images/navbarIcons/notif.svg'
 import './Navbar.scss'
 
 export default interface Iprops {
@@ -32,31 +32,42 @@ export const Navbar: React.FunctionComponent<Iprops> = ({ toggleHamburgerMenu, t
   return (
     <nav
       className="flex-row-wrap-withspace
+      tablet-max:pg-h-RSheader
+      tablet-max:pg-pr-15p
+      tablet-max:pg-pl-15p
+      tablet-max:pg-pt-0
+      tablet-max:pg-pb-0
+      pg-shadow-header
       fixed-on-top
       pg-h-header
       pg-bg-white
       pg-z-50
-      pg-shadow-header
-      tablet-max:pg-h-RSheader
-      tablet-max:pg-pt-0
-      tablet-max:pg-pb-0
-      tablet-max:pg-pr-15p
-      tablet-max:pg-pl-15p
       navbar"
     >
       <div className="pg-flex pg-flex-row right">
         <a onClick={e => {e.preventDefault(); toggleHamburgerMenu()}} 
-        className="pg-hidden
-        pg-items-center
-        pg-cursor-pointer
-        tablet-max:pg-flex
-        menuWrapper">
+          className="pg-hidden
+          tablet-max:pg-flex
+          pg-cursor-pointer
+          pg-items-center
+          menuWrapper"
+        >
           <Icon src={menu} className="pg-ml-6p menu" alt={`menu ${altIcon}`} />
         </a>
-        <a href="/" className="flex-center pg-w-sidebar tablet-max:pg-w-auto logoWrapper">
+        <a href="/" 
+          className="flex-center
+          pg-w-sidebar
+          tablet-max:pg-w-auto
+          logoWrapper">
           <Icon src={logo} className="logo" alt="pg-logo" />
         </a>
-        <div className="pg-flex pg-flex-row pg-flex-wrap pg-p-0 tablet-max:pg-hidden nav">
+        <div
+          className="pg-flex
+          tablet-max:pg-hidden
+          pg-flex-wrap
+          pg-flex-row
+          pg-p-0
+          nav">
           <NavLink to={`/fm`} className="item" activeClassName="current">
             <IconLink icon={fileCloudIcon} iconAlt={`File-cloud ${altIcon}`} label={t`میزبانی‌فایل`} />
           </NavLink>
@@ -68,8 +79,20 @@ export const Navbar: React.FunctionComponent<Iprops> = ({ toggleHamburgerMenu, t
           </NavLink>
         </div>
       </div>
-      <div className="pg-flex pg-items-center pg-ml-sidebar tablet-max:pg-ml-0 left">
-        <div className="pg-flex pg-flex-row pg-flex-wrap pg-overflow-hidden nav">
+      <div
+        className="pg-flex
+        pg-items-center
+        pg-ml-sidebar
+        tablet-max:pg-ml-0 
+        left"
+      >
+        <div
+          className="pg-flex
+          pg-flex-row
+          pg-flex-wrap
+          pg-overflow-hidden
+          nav"
+        >
           <IconLink icon={notifIcon} className="iconLink webIcon hasNotif" iconAlt={`Notif ${altIcon}`} />
           <Link to={`/vm/giftcard`}>
             <IconLink icon={financeIcon} className="iconLink webIcon" iconAlt={`Finance ${altIcon}`}/>
