9f525f8bc1b2c513aa006dc69ee48ee7cfd2e2dd
Alipanah <alipanah@Mehdi.local>
2019-06-23 15:24:37 +0430

    footer rafactoring

--- src/components/ui-elements/MainFooter/MainFooter.tsx
+++ src/components/ui-elements/MainFooter/MainFooter.tsx
@@ -9,7 +9,7 @@ export default interface Iprops {}
 export const MainFooter: React.FunctionComponent<Iprops> = () => (
 	<footer className={`pg-absolute pg-inset-x-0 pg-text-center ${styles.mainFooter}`}>
 		<div>Powered by <a className="pg-text-blue-400 pg-font-vMedium">PersianGig</a> IaaS Cloud Platform</div>
-		<div className={`pg-absolut mobile:pg-relative pg-left-0 ${styles.switcher}`}>
+		<div className={`pg-absolute mobile:pg-relative pg-left-0 ${styles.switcher}`}>
 			<LangSwitcher />
 		</div>
 	</footer>
